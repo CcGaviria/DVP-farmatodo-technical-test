38b517bb23d0962c2c3f64c9f596f8cb4212de6a
CcGaviria <cristian@gaviria.org>
2025-03-05 12:11:11 -0500

    Swagger is implemented

---
+++ farmatodo-user-service/src/main/java/com/gaviria/farmatodo_user_service/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.gaviria.farmatodo_user_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Farmatodo User Service API")
+                        .description("API para el manejo de usuarios de Farmatodo")
+                        .version("1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .name("BearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    
+    }
+}
--- farmatodo-user-service/src/main/java/com/gaviria/farmatodo_user_service/controllers/UserController.java
+++ farmatodo-user-service/src/main/java/com/gaviria/farmatodo_user_service/controllers/UserController.java
@@ -16,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gaviria.farmatodo_user_service.models.User;
 import com.gaviria.farmatodo_user_service.services.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Usuarios", description = "Operaciones CRUD de Usuarios")
+@SecurityRequirement(name = "BearerAuth")
 public class UserController {
     private final UserService userService;
     
@@ -27,21 +32,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Crear un usuario")
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
         return ResponseEntity.ok(userService.createUser(user));
     }
 
+    @Operation(summary = "Actualizar un usuario")
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable UUID id, @RequestBody User user) {
         return ResponseEntity.ok(userService.updateUser(id, user));
     }
 
+    @Operation(summary = "Obtener todos los usuarios")
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(userService.getUsers());
     }
 
+    @Operation(summary = "Obtener un usuario por ID")
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.getUserById(id));
