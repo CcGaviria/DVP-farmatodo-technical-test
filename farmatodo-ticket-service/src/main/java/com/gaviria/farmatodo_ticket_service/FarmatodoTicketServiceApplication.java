6ea77bbade8acc2c9cb3cb0ba1f30126a76b69ed
cristian@gaviria.org <Cristian Gaviria>
2025-03-05 09:26:46 -0500

    Generate the ticket microservice

---
+++ farmatodo-ticket-service/src/main/java/com/gaviria/farmatodo_ticket_service/FarmatodoTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gaviria.farmatodo_ticket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmatodoTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmatodoTicketServiceApplication.class, args);
+	}
+
+}
