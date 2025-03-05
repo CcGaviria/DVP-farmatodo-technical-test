4b7baf310f806afb17710f3a3515c1e88867c5c4
CcGaviria <cristian@gaviria.org>
2025-03-05 09:24:00 -0500

    Generate the user microservice

---
+++ farmatodo-user-service/src/main/java/com/gaviria/farmatodo_user_service/FarmatodoUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gaviria.farmatodo_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmatodoUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmatodoUserServiceApplication.class, args);
+	}
+
+}
