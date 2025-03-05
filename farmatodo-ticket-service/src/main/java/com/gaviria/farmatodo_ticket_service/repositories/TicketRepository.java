e77420cc00c691de881478f350980e3c66867943
cristian@gaviria.org <Cristian Gaviria>
2025-03-05 17:44:27 -0500

    The Filters in the query getAllTickets are implemented

--- farmatodo-ticket-service/src/main/java/com/gaviria/farmatodo_ticket_service/repositories/TicketRepository.java
+++ farmatodo-ticket-service/src/main/java/com/gaviria/farmatodo_ticket_service/repositories/TicketRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.gaviria.farmatodo_ticket_service.enums.TicketStatus;
 import com.gaviria.farmatodo_ticket_service.models.Ticket;
 
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
 
     Page<Ticket> findAll(Pageable pageable);
+
+    Page<Ticket> findByUserId(UUID userId, Pageable pageable);
+
+    Page<Ticket> findByStatus(TicketStatus status, Pageable pageable);
+
+    Page<Ticket> findByUserIdAndStatus(UUID userId, TicketStatus status, Pageable pageable);
     
 }
